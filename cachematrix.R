8c6e49d88dd61acb5e0e5486b82837cde3c13eda
zico882 <izeeko@gmail.com>
2015-09-26 21:04:27 +0300

    Solution to assignment
    Code for makeCacheMatrix and cacheSolve written and tested.
    
    Extensive comment also included in the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera: R programming Assignment 2
+## SCOPING RULES
+## =====================================
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix and creates a special "matrix" object 
+## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL                 # Assign NULL to inv object
+        }
+        get <- function() x                  #  get matrix x
+        setInv <- function(solve) {
+                inv <<- solve                # solve for matrix inverse
+        }
+        getInv <- function() inv             # get solved matrix inverse
+        
+        list(set = set, get = get,           # create list from function
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by ‘makeCacheMatrix’
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()                     # get inv matrix (NULL if not calculated)
+        if(!is.null(inv)) {                   # check if inv of matrix exists
+                message("getting cached data")
+                return(inv)
+        }
+        
+        data <- x$get()                       # get matrix 'x' 
+        inv <- solve(data, ...)               # solve inverse of matrix
+        x$setInv(inv)                         # Saves the inverse of matrix 'x' (inv)
+        inv                                   # Returns a matrix inv, the inverse of 'x'
 }
